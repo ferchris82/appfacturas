018b121c8e81acf708f9113e96d1898fe6853e12
Christian <ferchris82@hotmail.com>
2023-12-06 18:28:33 -0500

    Versión 0.1

---
+++ src/org/chrisferdev/appfacturas/domain/ItemFactura.java
@@ -0,0 +1,20 @@
+package org.chrisferdev.appfacturas.domain;
+
+public class ItemFactura {
+
+    private int cantidad;
+    private Producto producto;
+
+    public ItemFactura(int cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
